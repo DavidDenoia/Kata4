77a81432dd04ea562c774df5db42152e69827b17
David <David@DESKTOP-S4EGEPD>
2022-11-29 16:31:17 +0000

    Kata4

---
+++ src/Main/Kata4.java
@@ -0,0 +1,43 @@
+package Main;
+
+import Model.Histogram;
+import Model.Mail;
+import View.HistogramDisplay;
+import View.MailHistogramBuilder;
+import View.MailListReader;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Kata4 {
+    
+    private String fileName;
+    private List<Mail> mailList;
+    private Histogram<String> histogram;
+
+    public static void main(String[] args) {
+        Kata4 kata4 = new Kata4();
+        kata4.control();
+    }
+    
+    private void control() {
+        input();
+        process();
+        output();
+    }
+    
+    private void input() {
+        System.out.println("Introduce el nombre del fichero: ");
+        fileName = new Scanner(System.in).nextLine();
+        mailList = MailListReader.read(fileName);
+    }
+    
+    private void process() {
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+
+    private void output() {
+        new HistogramDisplay(histogram).execute();
+    }
+    
+}
